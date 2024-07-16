b1ecab7a843fb7633f175c07833ecc88e9edc384
nuhaminsolomon <nuhamin.sele@gmail.com>
2024-07-16 12:36:24 +0300

    Update basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,1 +1,16 @@
+# basic_operations.py
+
+# Define variables
+number1 = 10
+number2 = 5
+
+# Perform arithmetic operations
+addition_result = number1 + number2
+subtraction_result = number1 - number2
+multiplication_result = number1 * number2
+
+# Print results
+print(f"Addition of {number1} and {number2} is {addition_result}")
+print(f"Subtraction of {number1} and {number2} is {subtraction_result}")
+print(f"Multiplication of {number1} and {number2} is {multiplication_result}")
 
